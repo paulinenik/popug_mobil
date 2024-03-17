deace08fcdc62a8215ca06b0c97173fd1a02a377
Polina Nikitina <polinaarcher@gmail.com>
2024-03-17 18:58:55 +0300

    Billing: produce TransactionCreated event

--- billing/src/balance/services/transaction_creator.py
+++ billing/src/balance/services/transaction_creator.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
+from app.kafka.producer import Producer
 from app.services import BaseService
 from balance.models.transaction import Transaction
 
@@ -13,6 +14,23 @@ class TransactionCreator(BaseService):
     user: "User"
     type: str
     amount: int
+    task: "Task | None" = None
 
     def act(self) -> "Transaction":
-        return Transaction.objects.create(user=self.user, type=self.type, amount=self.amount)
+        return Transaction.objects.create(user=self.user, type=self.type, amount=self.amount, task=self.task)
+
+    def _create_transaction(self) -> "Transaction":
+        return Transaction.objects.create(user=self.user, type=self.type, amount=self.amount, task=self.task)
+
+    def _produce_event(self) -> None:
+        Producer(topic="transactions-stream", event="TransactionCreated", data=self._get_event_data())()
+
+    def _get_event_data(self) -> dict:
+        data = {
+            "user_id": self.user.public_id,
+            "type": self.type,
+            "amount": self.amount,
+        }
+        if self.task:
+            data["task_id"] = self.task.public_id
+        return data
